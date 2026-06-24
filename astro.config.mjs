7b906ebd19703012a079ff65cc26dbfde8a26dd1
M1thieu <18742831+M1thieu@users.noreply.github.com>
2026-06-24 22:50:39 +0200

    Corrige le domaine canonique vers passerelles-alsace.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://passerelles-68600.netlify.app',
+  site: 'https://passerelles-alsace.netlify.app',
 });
